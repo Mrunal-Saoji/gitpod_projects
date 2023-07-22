0e7539e0f34d8dd368cbd6bd52eddad62c77db4a
Mrunal Saoji <mrunalsaoji@gmail.com>
2023-07-22 19:49:31 +0000

    three js project

--- react_projects/threejsproject/src/App.jsx
+++ react_projects/threejsproject/src/App.jsx
@@ -6,7 +6,21 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
     </BrowserRouter>
   )
 }
